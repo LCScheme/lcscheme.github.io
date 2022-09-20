93f61f2d3d9c3498a02e703b22bc364ddee61beb
Hikaru Chang <i@rua.moe>
2022-09-21 02:56:08 +0800

    Deploy

---
+++ p__404.c9c73fe3.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunklcscheme=self.webpackChunklcscheme||[]).push([[571],{47680:function(E,n,u){u.r(n);var t=u(67294),e=u(30028),s=u(85893),c=function(){return(0,t.useEffect)(function(){e.history.push("/")},[]),(0,s.jsx)(s.Fragment,{})};n.default=c}}]);
